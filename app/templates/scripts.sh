bb02ec281117d6dc0edc3a399902dd00cea0690c
Joey Baker <joey@byjoeybaker.com>
2015-05-07 13:32:55 -0700

    script: linter #cleanup

--- app/templates/scripts.sh
+++ app/templates/scripts.sh
@@ -8,7 +8,8 @@ IFS=$'\n\t'
 # get the last word with awk, which is the version number
 # if that version number is greater than 7, we're good,
 # else, we need to install coreutils
-if [ $(echo "$(sort --version | grep '(GNU' | awk 'NF>1{print $NF}') > 7" | bc) != 1 ]; then
+# ensure the gnu version of sort is avaliable
+if [ $(PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" && echo "$(sort --version | grep '(GNU' | awk 'NF>1{print $NF}') > 7" | bc) != 1 ]; then
   echo "Installing coreutils to give you a modern shell experience"
   brew install coreutils
 fi
@@ -41,26 +42,33 @@ function find_last_git_tag(){
 # based on https://github.com/tj/git-extras/blob/master/bin/git-changelog
 function generate_git_changelog(){
   GIT_LOG_OPTS="--no-merges"
-  local DATE=$(date +'%Y-%m-%d')
+  local DATE
+  DATE=$(date +'%Y-%m-%d')
   local HEAD='## '
 
   # get the commits between the most recent tag and the second most recent
-  local lasttag=$(find_last_git_tag)
-  local version=$(git describe --tags --abbrev=0 "$lasttag" 2>/dev/null)
-  local previous_version=$(git describe --tags --abbrev=0 "$lasttag^" 2>/dev/null)
+  local lasttag
+  lasttag=$(find_last_git_tag)
+  local version
+  version=$(git describe --tags --abbrev=0 "$lasttag" 2>/dev/null)
+  local previous_version
+  previous_version=$(git describe --tags --abbrev=0 "$lasttag^" 2>/dev/null)
   # if we don't have a previous version to look at
   if test -z "$version"; then
     local head="$HEAD$DATE"
-    local changes=$(git log $GIT_LOG_OPTS --pretty="format:* %s%n" 2>/dev/null)
+    local changes
+    changes=$(git log $GIT_LOG_OPTS --pretty="format:* %s%n" 2>/dev/null)
   # the more common case, there's a version to git the changes betwen
   else
     local head="$HEAD$version | $DATE"
     # tail to get remove the first line, which will always just be the version commit
     # awk to remove empty lines
-    local changes=$(tail -n +2 <<< "$(git log $GIT_LOG_OPTS --pretty="format:* %s%n" "$previous_version..$version" 2>/dev/null)" | awk NF)
+    local changes
+    changes=$(tail -n +2 <<< "$(git log $GIT_LOG_OPTS --pretty="format:* %s%n" "$previous_version..$version" 2>/dev/null)" | awk NF)
   fi
 
-  local CHANGELOG=$(find_changelog_file)
+  local CHANGELOG
+  CHANGELOG=$(find_changelog_file)
 
   echo "Editing $CHANGELOG"
   # insert the changes after the header (assumes markdown)
