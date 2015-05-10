0f2cc70a9f6e3c2bd45c1cca0616c66a7d14393e
Joey Baker <joey@byjoeybaker.com>
2015-05-10 11:26:10 -0700

    dev: add many more tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,57 +5,151 @@ var path = require('path')
   , helpers = require('yeoman-generator').test
   , fs = require('fs')
   , testDir = path.join(__dirname, 'temp')
-
-test('iojs generator creation', function creationTest (t) {
-  var promptAnswers = {
+  , _ = require('lodash')
+  , defaultProptAnswers = {
       name: 'mymodule'
-      , description: 'awesome module'
       , pkgName: false
-      , license: 'Artistic 2.0'
+      , description: 'awesome module'
       , homepage: 'https://github.com'
+      , keywords: 'keyword1,keyword2,keyword3'
+      , license: 'Artistic 2.0'
       , githubUsername: 'octocat'
       , authorName: 'Octo Cat'
       , authorEmail: 'octo@example.com'
       , authorUrl: 'http://yeoman.io'
-      , keywords: 'keyword1,keyword2,keyword3'
+      , isServer: true
+      , isBrowser: true
+      , isCLI: false
+      , extension: '.js'
+      , isPrivate: true
     }
+  , defaultExpectedFiles = [
+      'package.json'
+      , '.editorconfig'
+      , '.eslintrc'
+      , '.gitignore'
+      , '.npmignore'
+      , '.npmrc'
+      , '.travis.yml'
+      , 'CHANGELOG.md'
+      , 'CONTRIBUTING.md'
+      , 'LICENSE'
+      , 'README.md'
+      , 'index.js'
+      , 'scripts.sh'
+      , 'test'
+    ]
+  , assertFilesInDir = function assertFilesInDir (t, dir, expectedFiles, callback) {
+    fs.readdir(dir, function readFilesInDir (err, files) {
+      t.error(err, t.name + ' should be able to read ' + dir)
+
+      expectedFiles.forEach(function forEachExpectedFile (file) {
+        t.ok(
+          files.indexOf(file) > -1
+          , t.name + ' outputs ' + dir + file
+        )
+      })
+
+      if (callback) callback(err, files)
+    })
+  }
+
+test('iojs generator creation: defaults', function creationTest (t) {
+  var promptAnswers = _.clone(defaultProptAnswers)
     , runTests
 
   runTests = function runTests () {
-      var expected = [
-        'package.json'
-        , '.editorconfig'
-        , '.gitignore'
-        , '.eslintrc'
-        , '.npmignore'
-        , '.npmrc'
-        , 'scripts.sh'
-        , '.travis.yml'
-        , 'README.md'
-        , 'CONTRIBUTING.md'
-        , 'CHANGELOG.md'
-        , 'LICENSE'
-      ]
+      var expected = _.clone(defaultExpectedFiles)
+        , expectedInTest = ['test.js']
 
-      // 3 assertations + the count of files we expect
-      t.plan(3 + expected.length)
+      // 4 assertations + the count of files we expect
+      t.plan(9 + expected.length + expectedInTest.length)
 
-      fs.readdir(testDir, function filesInTestDir (err, files) {
-        t.error(err, 'should be able to read the test dir')
-
-        expected.forEach(function forEachExpectedFile (file) {
-          t.ok(
-            files.indexOf(file) > -1
-            , 'outputs ' + file
-          )
-        })
-      })
+      assertFilesInDir(t, testDir, expected)
+      assertFilesInDir(t, path.join(testDir, 'test'), expectedInTest)
 
       fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
+        var pkgContents = file.toString()
         t.error(err, 'should be able to read the package.json')
         t.ok(
-           /"name": "mymodule"/.test(file.toString())
+           /"name": "mymodule"/.test(pkgContents)
           , 'adds the module name to package.json'
+        )
+        t.ok(
+           /"test-browser":/.test(pkgContents)
+          , 'adds a `test-browser` script'
+        )
+        t.ok(
+           /"test-server":/.test(pkgContents)
+          , 'adds a `test-server` script'
+        )
+        t.ok(
+           /"tdd-browser":/.test(pkgContents)
+          , 'adds a `tdd-browser` script'
+        )
+        t.ok(
+           /"tdd-server":/.test(pkgContents)
+          , 'adds a `tdd-server` script'
+        )
+        t.ok(
+           /npm_release private/.test(pkgContents)
+          , 'passes the access level to npm_run'
+        )
+      })
+    }
+
+  helpers.run(path.join(__dirname, '..', 'app', 'index.js'))
+    .inDir(testDir)
+    .withPrompts(promptAnswers)
+    .on('end', runTests)
+})
+
+test('iojs generator creation: CLI only', function creationTest (t) {
+  var promptAnswers = _.clone(defaultProptAnswers)
+    , runTests
+
+  promptAnswers.isServer = false
+  promptAnswers.isBrowser = false
+  promptAnswers.isCLI = true
+  promptAnswers.name = 'my-module'
+
+  runTests = function runTests () {
+      var expected = _.clone(defaultExpectedFiles)
+        , expectedInBin = [promptAnswers.name]
+      expected.push('bin')
+
+      // 4 assertations + the count of files we expect
+      t.plan(9 + expected.length + expectedInBin.length)
+
+      assertFilesInDir(t, testDir, expected)
+      assertFilesInDir(t, path.join(testDir, 'bin'), expectedInBin)
+
+      fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
+        var pkgContents = file.toString()
+        t.error(err, 'should be able to read the package.json')
+        t.ok(
+           /"name": "my-module"/.test(pkgContents)
+          , 'adds the module name to package.json'
+        )
+        t.ok(
+           /"my-module": "bin\/my-module"/.test(pkgContents)
+          , 'sets the bin script'
+        )
+        t.ok(
+           /"test":/.test(pkgContents)
+          , 'adds a `test` script'
+        )
+        t.ok(
+           /"tdd":/.test(pkgContents)
+          , 'adds a `tdd` script'
+        )
+        t.notOk(
+           /"tdd-server":/.test(pkgContents)
+          , 'does not add a `tdd-server` script'
+        )
+        t.notOk(
+           /"tdd-browser":/.test(pkgContents)
+          , 'does not add a `tdd-browser` script'
         )
       })
     }
