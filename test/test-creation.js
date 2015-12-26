9c862606245fd09f98378bcc0af3f0bffe5682dd
Joey Baker <joey@byjoeybaker.com>
2015-12-26 15:42:56 -0500

    internal: #cleanup test for w/ some es6

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,113 +1,116 @@
-var path = require('path')
-  , test = require('tape')
-  , helpers = require('yeoman-generator').test
-  , fs = require('fs')
-  , testDir = path.join(__dirname, 'temp')
-  , _ = require('lodash')
-  , defaultProptAnswers = {
-      name: 'mymodule'
-      , pkgName: false
-      , description: 'awesome module'
-      , homepage: 'https://github.com'
-      , keywords: 'keyword1,keyword2,keyword3'
-      , license: 'Artistic 2.0'
-      , githubUsername: 'octocat'
-      , authorName: 'Octo Cat'
-      , authorEmail: 'octo@example.com'
-      , authorUrl: 'http://yeoman.io'
-      , isServer: true
-      , isBrowser: true
-      , isCLI: false
-      , extension: '.js'
-      , isPrivate: true
-    }
-  , defaultExpectedFiles = [
-      'package.json'
-      , '.babelrc'
-      , '.editorconfig'
-      , '.eslintrc'
-      , '.gitignore'
-      , '.npmignore'
-      , '.npmrc'
-      , '.travis.yml'
-      , 'CHANGELOG.md'
-      , 'CONTRIBUTING.md'
-      , 'LICENSE'
-      , 'README.md'
-      , 'index.js'
-      , 'scripts.sh'
-      , 'test'
-    ]
-  , assertFilesInDir = function assertFilesInDir (t, dir, expectedFiles, callback) {
-    fs.readdir(dir, function readFilesInDir (err, files) {
-      t.error(err, t.name + ' should be able to read ' + dir)
+const path = require('path')
+const test = require('tape')
+const helpers = require('yeoman-generator').test
+const fs = require('fs')
+const testDir = path.join(__dirname, 'temp')
+const _ = require('lodash')
 
-      expectedFiles.forEach(function forEachExpectedFile (file) {
-        t.ok(
-          files.indexOf(file) > -1
-          , t.name + ' outputs ' + dir + file
-        )
-      })
+const defaultProptAnswers = {
+  name: 'mymodule'
+  , pkgName: false
+  , description: 'awesome module'
+  , homepage: 'https://github.com'
+  , keywords: 'keyword1,keyword2,keyword3'
+  , license: 'Artistic 2.0'
+  , githubUsername: 'octocat'
+  , authorName: 'Octo Cat'
+  , authorEmail: 'octo@example.com'
+  , authorUrl: 'http://yeoman.io'
+  , isServer: true
+  , isBrowser: true
+  , isCLI: false
+  , extension: '.js'
+  , isPrivate: true
+}
 
-      if (callback) callback(err, files)
+const defaultExpectedFiles = [
+  'package.json'
+  , '.babelrc'
+  , '.editorconfig'
+  , '.eslintrc'
+  , '.gitignore'
+  , '.npmignore'
+  , '.npmrc'
+  , '.travis.yml'
+  , 'CHANGELOG.md'
+  , 'CONTRIBUTING.md'
+  , 'LICENSE'
+  , 'README.md'
+  , 'index.js'
+  , 'scripts.sh'
+  , 'test'
+]
+
+const assertFilesInDir = function assertFilesInDir (t, dir, expectedFiles, callback) {
+  fs.readdir(dir, function readFilesInDir (err, files) {
+    t.error(err, t.name + ' should be able to read ' + dir)
+
+    expectedFiles.forEach(function forEachExpectedFile (file) {
+      t.ok(
+        files.indexOf(file) > -1
+        , t.name + ' outputs ' + dir + file
+      )
     })
-  }
+
+    if (callback) callback(err, files)
+  })
+}
 
 test('iojs generator creation: defaults', function creationTest (t) {
-  var promptAnswers = _.clone(defaultProptAnswers)
-    , runTests = function runTests () {
-      var expected = _.clone(defaultExpectedFiles)
-        , expectedInTest = ['test.js']
+  const promptAnswers = _.clone(defaultProptAnswers)
+  const runTests = function runTests () {
+    const expected = _.clone(defaultExpectedFiles)
+    const expectedInTest = ['test.js']
 
-      // 4 assertations + the count of files we expect
-      t.plan(12 + expected.length + expectedInTest.length)
+    // 4 assertations + the count of files we expect
+    t.plan(12 + expected.length + expectedInTest.length)
 
-      assertFilesInDir(t, testDir, expected)
-      assertFilesInDir(t, path.join(testDir, 'test'), expectedInTest)
+    assertFilesInDir(t, testDir, expected)
+    assertFilesInDir(t, path.join(testDir, 'test'), expectedInTest)
 
-      fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
-        var pkgContents = file.toString()
-        t.error(err, 'should be able to read the package.json')
-        t.ok(
-           /"name": "mymodule"/.test(pkgContents)
-          , 'adds the module name to package.json'
-        )
-        t.ok(
-           /"test-browser":/.test(pkgContents)
-          , 'adds a `test-browser` script'
-        )
-        t.ok(
-           /"test-server":/.test(pkgContents)
-          , 'adds a `test-server` script'
-        )
-        t.ok(
-           /"tdd-browser":/.test(pkgContents)
-          , 'adds a `tdd-browser` script'
-        )
-        t.ok(
-           /"tdd-server":/.test(pkgContents)
-          , 'adds a `tdd-server` script'
-        )
-        t.ok(
-           /npm_release restricted/.test(pkgContents)
-          , 'passes the access level to npm_run'
-        )
-        t.ok(
-           /"babel-plugin-closure-elimination":/.test(pkgContents)
-          , 'includes the closure-elimination plugin'
-        )
-      })
+    fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
+      const pkgContents = file.toString()
+      t.error(err, 'should be able to read the package.json')
+      t.ok(
+         /"name": "mymodule"/.test(pkgContents)
+        , 'adds the module name to package.json'
+      )
+      t.ok(
+         /"test-browser":/.test(pkgContents)
+        , 'adds a `test-browser` script'
+      )
+      t.ok(
+         /"test-server":/.test(pkgContents)
+        , 'adds a `test-server` script'
+      )
+      t.ok(
+         /"tdd-browser":/.test(pkgContents)
+        , 'adds a `tdd-browser` script'
+      )
+      t.ok(
+         /"tdd-server":/.test(pkgContents)
+        , 'adds a `tdd-server` script'
+      )
+      t.ok(
+         /npm_release restricted/.test(pkgContents)
+        , 'passes the access level to npm_run'
+      )
+      t.ok(
+         /"babel-plugin-closure-elimination":/.test(pkgContents)
+        , 'includes the closure-elimination plugin'
+      )
+    })
 
-      fs.readFile(path.join(testDir, '.travis.yml'), function readTravisYml (err, file) {
-        var travisContents = file.toString()
-        t.error(err, 'should be able to read .travis.yml')
+    fs.readFile(path.join(testDir, '.travis.yml'), function readTravisYml (err, file) {
+      const travisContents = file.toString()
+      t.error(err, 'should be able to read .travis.yml')
 
-        t.ok(
-          /export DISPLAY=\:99\.0; sh \-e \/etc\/init.d\/xvfb start/.test(travisContents)
-          , 'adds a before script, that starts xvfb for browser tests'
-        )
-      })
-    }
+      t.ok(
+        /export DISPLAY=\:99\.0; sh \-e \/etc\/init.d\/xvfb start/.test(travisContents)
+        , 'adds a before script, that starts xvfb for browser tests'
+      )
+    })
+  }
 
   helpers.run(path.join(__dirname, '..', 'app', 'index.js'))
     .inDir(testDir)
@@ -116,8 +119,7 @@ test('iojs generator creation: defaults', function creationTest (t) {
 })
 
 test('iojs generator creation: CLI only', function creationTest (t) {
-  var promptAnswers = _.clone(defaultProptAnswers)
-    , runTests
+  const promptAnswers = _.clone(defaultProptAnswers)
 
   promptAnswers.isServer = false
   promptAnswers.isBrowser = false
@@ -126,10 +128,11 @@ test('iojs generator creation: CLI only', function creationTest (t) {
 
   /* eslint-disable no-shadow */
   // eslint bug
-  runTests = function runTests () {
+  const runTests = function runTests () {
     /* eslint-enable no-shadow */
-    var expected = _.clone(defaultExpectedFiles)
-      , expectedInBin = [promptAnswers.name]
+    const expected = _.clone(defaultExpectedFiles)
+    const expectedInBin = [promptAnswers.name]
+
     expected.push('bin')
 
     // assertations + the count of files we expect
@@ -139,7 +142,7 @@ test('iojs generator creation: CLI only', function creationTest (t) {
     assertFilesInDir(t, path.join(testDir, 'bin'), expectedInBin)
 
     fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
-        var pkgContents = file.toString()
+        const pkgContents = file.toString()
         t.error(err, 'should be able to read the package.json')
         t.ok(
            /"name": "my-module"/.test(pkgContents)
@@ -172,9 +175,9 @@ test('iojs generator creation: CLI only', function creationTest (t) {
       })
 
     fs.readFile(path.join(testDir, 'bin', promptAnswers.name), function readPackageJson (err, file) {
-        var cliContents
         t.error(err, 'should be able to read the bin script')
-        cliContents = file.toString()
+
+        const cliContents = file.toString()
 
         t.ok(
            cliContents.indexOf('.usage(\'' + promptAnswers.description + '\')') > -1
@@ -195,16 +198,15 @@ test('iojs generator creation: CLI only', function creationTest (t) {
 })
 
 test('iojs generator creation: no es6', function creationTest (t) {
-  var promptAnswers = _.clone(defaultProptAnswers)
-    , runTests
+  const promptAnswers = _.clone(defaultProptAnswers)
 
   promptAnswers.isEs6 = false
 
   /* eslint-disable no-shadow */
   // eslint bug
-  runTests = function runTests () {
+  const runTests = function runTests () {
     /* eslint-enable no-shadow */
-    var expected = _.clone(defaultExpectedFiles)
+    const expected = _.clone(defaultExpectedFiles)
     expected.splice(expected.indexOf('.babelrc'), 1)
 
     // assertations + the count of files we expect
@@ -213,9 +215,8 @@ test('iojs generator creation: no es6', function creationTest (t) {
     assertFilesInDir(t, testDir, expected)
 
     fs.readFile(path.join(testDir, 'package.json'), function readPackageJson (err, file) {
-        var pkgContents
         t.error(err, 'should be able to read the package.json')
-        pkgContents = file.toString()
+        const pkgContents = file.toString()
 
         t.ok(
           /"start": "node index/.test(pkgContents)
@@ -236,9 +237,9 @@ test('iojs generator creation: no es6', function creationTest (t) {
       })
 
     fs.readFile(path.join(testDir, '.eslintrc'), function readPackageJson (err, file) {
-        var eslintrcContents
         t.error(err, 'should be able to read the .eslintrc')
-        eslintrcContents = file.toString()
+
+        const eslintrcContents = file.toString()
 
         t.notOk(
            /"es6": true/.test(eslintrcContents)
